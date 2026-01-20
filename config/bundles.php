6ea39808e3a76064eaeb9b8694b9e2edb8f5493c
Eva Valenti <b.beta.crystal@gmail.com>
2026-01-20 14:56:13 +0100

    /

--- config/bundles.php
+++ config/bundles.php
@@ -11,9 +11,6 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-<<<<<<< HEAD
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-=======
->>>>>>> fdcd32e (premier test menu latteral)
 ];
