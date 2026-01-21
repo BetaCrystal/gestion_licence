af1a0d43bef2b314446840d568af265a7beb5cdc
Kecy Platel <kecy.platel@gmail.com>
2026-01-21 09:35:08 +0100

    résolution conflits

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -52,4 +52,35 @@ class CourseController extends AbstractController
             'form' => $form,
         ]);
     }
+
+    #[Route(path: '/twig/change_course', name: 'app_change_course', methods: ['GET'])]
+    public function changeCourse(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $course = new Course();
+
+        // Pre-fill dates if date parameter is provided
+        $dateStr = $request->query->get('date');
+        if ($dateStr) {
+            $selectedDate = new \DateTime($dateStr);
+            $course->setStartDate($selectedDate);
+            // Set end date to the same day or next day, adjust as needed
+            /*$endDate = clone $selectedDate;
+            $endDate->setTime(23, 59, 59); // End of the day
+            $course->setEndDate($endDate);*/
+        }
+
+        $form = $this->createForm(CourseForm::class, $course);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($course);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_course');
+        }
+
+        return $this->render('admin/form/form_course.html.twig', [
+            'form' => $form,
+        ]);
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-<<<<<<< HEAD
+
     "./templates/**/*.html.twig",
     "./assets/**/*.js"
-=======
-    "./assets/**/*.js",
-    "./templates/**/*.html.twig",
-    "./assets/**/*.css"
->>>>>>> fdcd32e (premier test menu latteral)
+
   ],
   theme: {
     extend: {},
