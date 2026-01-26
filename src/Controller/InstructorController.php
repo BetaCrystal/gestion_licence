5fd8ab840db08172060428aed248aa7e794f7fd9
Kecy Platel <kecy.platel@gmail.com>
2026-01-26 13:14:26 +0100

    ajout du bouton d'ajout d'un enseignant et du filtre des enseignants.

--- src/Controller/InstructorController.php
+++ src/Controller/InstructorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\InstructorFilterForm;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,10 +13,14 @@ use App\Repository\InstructorRepository;
 final class InstructorController extends AbstractController
 {
     #[Route(path: '/list_instructor', name: 'instructors', methods: ['GET'])]
-    public function findInstructors( InstructorRepository $InstructorRepository): Response
+    public function findInstructors( Request $request, InstructorRepository $InstructorRepository): Response
     {
-        return $this->render('instructor/instructor_list.html.twig', [
-            'instructor' => $InstructorRepository->findAllInstructor(),
-        ]);
-    }
+        $form = $this->createForm(InstructorFilterForm::class);
+        $form->handleRequest($request);
+
+            return $this->render('instructor/instructor_list.html.twig', [
+                'instructor' => $InstructorRepository->findAllInstructor(),
+                'form' => $form->createView(),
+            ]);
+        }
 }
