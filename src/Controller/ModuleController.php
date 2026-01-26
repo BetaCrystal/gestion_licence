aa7324b586a7050e0dc5d18dae2c785f9bb4cb1a
Eva Valenti <b.beta.crystal@gmail.com>
2026-01-26 16:47:33 +0100

    form module

--- src/Controller/ModuleController.php
+++ src/Controller/ModuleController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Module;
 use App\Form\ModuleForm;
+use App\Entity\TeachingBlock;
 
 final class ModuleController extends AbstractController
 {
@@ -24,13 +25,38 @@ final class ModuleController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            $form->getData();
             $this->addFlash('success', 'Module mis à jour avec succès !');
         }
 
         return $this->render('module/view_module.html.twig', [
             'moduleForm' => $form->createView(),
             'module' => $module,
+        ]);
+    }
+
+    #[Route(path:'/twig/add_module{block_id}', name:'app_add_module', methods:['GET','POST'])]
+    public function addModule(Request $request, ?int $block_id = null): Response
+    {
+        $module = new Module();
+        $block = new TeachingBlock();
+
+        if ($block_id) {
+            $module->setTeachingBlock($block);
+        }
+
+        $form = $this->createForm(ModuleForm::class, $module);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $module = $form->getData();
+            // Here you would typically save the new module to the database
+            $this->addFlash('success', 'Nouveau module ajouté avec succès !');
+            return $this->redirectToRoute('app_modules');
+        }
+
+        return $this->render('module/add_module.html.twig', [
+            'moduleForm' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/ModuleForm.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\TeachingBlock;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use App\Entity\Module;
+
+class ModuleForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('teaching_block', EntityType::class, [
+            'class' => TeachingBlock::class,
+            'disabled' => true,
+            'label' => 'Bloc enseignement',
+        ])
+        ->add('code', TextType::class, [
+            'label' => 'Code - champ obligatoire',
+            'required' => true,
+        ])
+        ->add('name', TextType::class, [
+            'label' => 'Nom - champ obligatoire',
+            'required' => true,
+        ])
+        ->add('amount_hours', NumberType::class, [
+            'label' => 'Nombre d\'heures',
+            'required' => false,
+        ])
+        ->add('parent', EntityType::class, [
+            'class' => Module::class,
+            'choice_label' => 'name',
+            'label' => 'Module parent',
+            'placeholder' => 'Sélectionnez le module parent',
+            'required' => false,
+        ])
+        ->add('description', TextareaType::class, [
+            'label' => 'Description - champ obligatoire',
+            'required' => true,
+        ])
+        ->add('capstone_project', CheckboxType::class, [
+            'label' => 'Module effectué sur le projet fil rouge',
+            'required' => false,
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([]);
+    }
+}
