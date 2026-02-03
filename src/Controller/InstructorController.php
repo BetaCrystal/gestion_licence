71f5bcf358afc270fc8c10004e506a6561615b3f
Nicolas Le-Goffic <legoffic.nicolas@gmail.com>
2026-02-03 15:59:04 +0100

    Création de la page d'ajout d'un enseignant

--- src/Controller/InstructorController.php
+++ src/Controller/InstructorController.php
@@ -58,6 +58,46 @@ final class InstructorController extends AbstractController
             'form' => $form->createView(),
         ]);
     }  
+
+    #[Route(path: '/enseignant/ajout', name: 'enseignant_ajout', methods: ['GET','POST'])]
+    public function ajoutInstructor(Request $request, InstructorRepository $repository, EntityManagerInterface $manager): Response
+    {
+        $instructor = new Instructor();
+                
+        $form = $this->createForm(InstructorInformationsForm::class, $instructor);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                try {
+                    
+                    // Afficher un message de succès
+                    $this->addFlash(
+                        'success',
+                        'Élément modifié en base de données.'
+                    );
+
+                    $manager->flush();
+
+                } catch (\Exception $exception) {
+                    $this->addFlash(
+                        'danger',
+                        'Une erreur est survenue lors de l\'enregistrement'
+                    );
+                }
+            } else {
+                // Afficher des messages d'erreur
+                $this->addFlash(
+                    'danger',
+                    'Formulaire invalide'
+                );
+            }
+        }
+
+
+        return $this->render('instructor/instructor_addInstructor.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }  
       #[Route('/list_instructor', name: 'instructors', methods: ['GET','POST'])] // Route de méthode
       public function list(Request $request, InstructorRepository $repo): Response
       {
