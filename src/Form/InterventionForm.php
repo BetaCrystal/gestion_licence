cf01ad6599ba672d598125500a78d3dab1d7d1cc
Eva Valenti <b.beta.crystal@gmail.com>
2026-02-09 17:13:06 +0100

    interventions css

--- src/Form/InterventionForm.php
+++ src/Form/InterventionForm.php
@@ -18,10 +18,12 @@ class InterventionForm extends AbstractType
         ->add('DateDebut', DateTimeType::class, [
                 'label' => 'Date de début',
                 'required' => false,
+                'attr' => ['class' => 'border border-slate-300 rounded-md px-3 py-1'],
         ])
         ->add('DateFin', DateTimeType::class, [
                 'label' => 'Date de fin',
                 'required' => false,
+                'attr' => ['class' => 'border border-slate-300 rounded-md px-3 py-1'],
         ])
         ->add('Module', EntityType::class, [
             'class' => Module::class,
@@ -29,9 +31,11 @@ class InterventionForm extends AbstractType
             'label' => 'Module',
             'placeholder' => 'Sélectionnez le module',
             'required' => false,
+            'attr' => ['class' => 'border border-slate-300 rounded-md px-3 py-1'],
         ])
         ->add('submit', SubmitType::class, [
             'label' => 'Filtrer',
+            'attr' => ['class' => 'px-4 py-2.5 text-blue-950 bg-yellow-500 font-medium text-base rounded-xl'],
         ]);
     }
 
